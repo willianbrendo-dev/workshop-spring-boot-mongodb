1568bdea2fb5e55ee3ccca9cf3633921bcc9485d
Willian Brendo <willianbrendo.dev@gmail.com>
2025-09-27 12:41:56 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/willianbrendo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/willianbrendo/workshopmongo/config/Instantiation.java
@@ -1,28 +1,49 @@
 package com.willianbrendo.workshopmongo.config;
 
+import java.time.Instant;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.willianbrendo.workshopmongo.domain.Post;
 import com.willianbrendo.workshopmongo.domain.User;
+import com.willianbrendo.workshopmongo.dto.UserDTO;
+import com.willianbrendo.workshopmongo.repositories.PostRepository;
 import com.willianbrendo.workshopmongo.repositories.UserRepository;
 
 @Component
-public class Instantiation  implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
-	private final UserRepository repository;
-	
-	public Instantiation(UserRepository repository) {
-        this.repository = repository;
-    }
-	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PostRepository postRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		repository.deleteAll(); // limpa antes, opcional
+		userRepository.deleteAll();
+		postRepository.deleteAll();// limpa antes, opcional
 
-        repository.save(new User(null, "Willian", "willian@example.com"));
-        repository.save(new User(null, "Maria", "maria@example.com"));
-        repository.save(new User(null, "João", "joao@example.com"));
+		User willian = new User(null, "Willian", "willian@example.com");
+		User maria = new User(null, "Maria", "maria@example.com");
+		User joao = new User(null, "João", "joao@example.com");
+
 		
+
+		Post post1 = new Post(null, Instant.now(), "Partiu Viagem!", "Vou viajar para São Paulo, Abraços",
+				new UserDTO(willian));
+		Post post2 = new Post(null, Instant.now().minusSeconds(86400), // Ex: um dia atrás
+				"Bom dia!", "Acordei hoje feliz, cheio de energia!", new UserDTO(maria) // Autor: Embutindo o DTO do Alex
+		);
+		
+		
+		
+		userRepository.saveAll(Arrays.asList(willian, maria, joao));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
 
 }
---
+++ src/main/java/com/willianbrendo/workshopmongo/domain/Post.java
@@ -0,0 +1,94 @@
+package com.willianbrendo.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.willianbrendo.workshopmongo.dto.UserDTO;
+
+@Document(collection = "posts") // Mapeia para a coleção "posts"
+public class Post implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private Instant date;
+	private String title;
+	private String body;
+	
+	// 🎯 Relacionamento de Documento Embutido (Embedded Document)
+    // O Spring Data irá persistir o UserDTO como um subdocumento dentro do Post
+    private UserDTO author; 
+    
+    public Post() {
+    }
+
+	public Post(String id, Instant date, String title, String body, UserDTO author) {
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public UserDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(UserDTO author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+}
